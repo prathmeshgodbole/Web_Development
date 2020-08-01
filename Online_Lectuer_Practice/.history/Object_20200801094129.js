36884f33fcb1b4710050a8ddb6e8b1aa60eea0c2
prathmeshgodbole <godbole611@gmail.com>
2020-08-01 09:47:26 +0530

    Object Basic

---
+++ Online_Lectuer_Practice/.history/Object_20200801094129.js
@@ -0,0 +1,21 @@
+var myCar = new Array();
+myCar[0] = "Audi";
+myCar[1] = 200;
+console.log(myCar);
+
+var myCar1 = new Object();
+myCar1.speed = 100;
+myCar1.name = "Dzire";
+myCar1.drive = function() { console.log("This is my old car...."); };
+myCar1.drive();
+console.log(myCar1.name);
+console.log(myCar1);
+
+//Converting string 
+var myCar2 = { name: "MG", speed: 150, brand: "Hector" };
+console.log(myCar2.name);
+console.log(myCar2.speed.toString());
+
+//Seting property value
+myCar1.Company = "Suzuki";
+console.log(myCar1);
---
+++ Online_Lectuer_Practice/.history/Object_20200801094511.js
@@ -0,0 +1,25 @@
+var myCar = new Array();
+myCar[0] = "Audi";
+myCar[1] = 200;
+console.log(myCar);
+document.write(myCar);
+
+var myCar1 = new Object();
+myCar1.speed = 100;
+myCar1.name = "Dzire";
+myCar1.drive = function() { console.log("This is my old car...."); };
+myCar1.drive();
+console.log(myCar1.name);
+console.log(myCar1);
+document.write(myCar1);
+
+//Converting integer into string
+var myCar2 = { name: "MG", speed: 150, brand: "Hector" };
+console.log(myCar2.name);
+console.log(myCar2.speed.toString());
+document.write(myCar2);
+
+//Seting property value
+myCar1.Company = "Suzuki";
+console.log(myCar1);
+document.write(myCar1);
